bc6eaf152a2d779e1eefc0f5af2cdc5acd2c1fc3
DeekshaB22 <121969309+DeekshaB22@users.noreply.github.com>
2023-01-10 10:35:46 +0530

    Update Shape detection.py

--- Shape detection.py
+++ Shape detection.py
@@ -1,4 +1,3 @@
-import numpy as np
 import cv2 as cv
 
 img = cv.imread('TCSR.jpg')
@@ -7,23 +6,22 @@ _,thresh = cv.threshold(gray, 240, 255, cv.THRESH_BINARY)
 cont,_ = cv.findContours(thresh, cv.RETR_TREE, cv.CHAIN_APPROX_NONE)
 
 cv.imshow("img", img)
-for cont in cont:
-    aprx = cv.approxPolyDP(cont, 0.01* cv.arcLength(cont, True), True)
-    cv.drawContours(img, [aprx], 0, (0, 0, 0), 5)
-    x = aprx.ravel()[0]
-    y = aprx.ravel()[1] - 5
+for c in cont:
+    aprx = cv.approxPolyDP(c, 0.01* cv.arcLength(c, True), True)
+    cv.drawContours(img, [aprx], 0, (0, 0, 0), 3)
+    x,y,w,h = cv.boundingRect(aprx)
+
     if len(aprx) == 3:
-        cv.putText(img, "Triangle", (x, y), cv.FONT_HERSHEY_COMPLEX, 0.5, (0, 0, 0))
+        cv.putText(img, "Triangle", (x+7, y-7), cv.FONT_HERSHEY_COMPLEX, 0.5, (0, 0, 0))
     elif len(aprx) == 4:
-        x1 ,y1, w, h = cv.boundingRect(aprx)
-        aspectRatio = float(w)/h
-        print(aspectRatio)
-        if aspectRatio >= 0.95 and aspectRatio <= 1.05:
-            cv.putText(img, "square", (x, y), cv.FONT_HERSHEY_COMPLEX, 0.5, (0, 0, 0))
+        x1 ,y1, w1, h1 = cv.boundingRect(aprx)
+        r = float(w1)/h1
+        if r >= 0.95 and r <= 1.05:
+            cv.putText(img, "Square", (x+7, y-7), cv.FONT_HERSHEY_COMPLEX, 0.5, (0, 0, 0))
         else:
-            cv.putText(img, "rectangle", (x, y), cv.FONT_HERSHEY_COMPLEX, 0.5, (0, 0, 0))
+            cv.putText(img, "Rectangle", (x+7, y-7), cv.FONT_HERSHEY_COMPLEX, 0.5, (0, 0, 0))
     else:
-        cv.putText(img, "Circle", (x, y), cv.FONT_HERSHEY_COMPLEX, 0.5, (0, 0, 0))
+        cv.putText(img, "Circle", (x+7, y-7), cv.FONT_HERSHEY_COMPLEX, 0.5, (0, 0, 0))
 
 
 cv.imshow("Shapes", img)
